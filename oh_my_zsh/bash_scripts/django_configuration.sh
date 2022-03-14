0be22192ebd91b4fbf5e9cf3240d6cadec3b7cd4
Amanda Furtado Brinhosa <42866976+amandafbri@users.noreply.github.com>
2022-03-14 14:37:24 -0300

    Update django_configuration.sh

--- oh_my_zsh/bash_scripts/django_configuration.sh
+++ oh_my_zsh/bash_scripts/django_configuration.sh
@@ -10,19 +10,25 @@ echo What environment do you want to access? Leave it blank for security.env
 read security_file
 export $(grep -v '^#' security$security_file.env | xargs)
 
-echo Do you want to runserver [r], shell [s], python script [py] or quit [q]? 
+echo Do you want to runserver [r], shell [s], python script [py], apply migrations [mg], show migrations [sm] or quit [q]? 
 read task
 if [ $task = r ]
 then
-	python3 central_data_provider/manage.py runserver
+	python3 your_repo/manage.py runserver
 elif [ $task = s ]
 then
-	python3 central_data_provider/manage.py shell
+	python3 your_repo/manage.py shell
 elif [ $task = py ]
 then
 	echo What is the path to your python script?
 	read python_script_path
-	python3 central_data_provider/manage.py shell < $python_script_path
+	python3 your_repo/manage.py shell < $python_script_path
+elif [ $task = mg ]
+then
+	python3 your_repo/manage.py migrate
+elif [ $task = sm ]
+then
+	python3 your_repo/manage.py showmigrations
 else [ $task = q ]
 	echo Quit
 fi
